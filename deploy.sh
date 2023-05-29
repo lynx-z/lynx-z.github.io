6b10c5bbc523764f279a615666a7e50b58d7693b
kai <scyjz8@gmail.com>
2023-05-29 12:11:36 +0100

    vue test

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+it commit -m 'New Deployment'
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/lynx-z/" : "/",
+})
