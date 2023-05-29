b789fd26797c90d5302fd805448ece2a96aaefef
Juekai ZHANG <85024355+lynx-z@users.noreply.github.com>
2023-05-29 12:20:11 +0100

    Delete vue.config.js

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  // publicPath: process.env.NODE_ENV === "production" ? "/lynx-z/" : "/",
-  publicPath: '/lynx-z/'
-
-})
