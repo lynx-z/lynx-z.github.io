258ed03af4173af190556c79d0cf93430c7ae4b0
kai <scyjz8@gmail.com>
2023-06-01 11:56:44 +0100

    add picture

--- src/views/weeks/week1.vue
+++ src/views/weeks/week1.vue
@@ -3,12 +3,13 @@
       <br><br>
       <h1>Week 1 (2023/5/29 - 2023/6/4)</h1><br>
 
+      <!--  -->
       <h2> Thoughts & Ideas</h2>
 
       <br>Art Style
       <ul>
         <li>2D game</li>
-        <li>Pixel? hard to draw, but easy to uniform.</li>
+        <li>Pixel? hard to draw, but easy to uniform. Pixel Tool: Aseprite</li>
         <li>Other art style: may be use AI generated element</li>
       </ul>
 
@@ -17,6 +18,15 @@
         <li>Sepreate Game, select which one to play</li>
         <li>Story Mode, may be set a simple story to connect all story</li>
       </ul>
+
+      
+      <!--  -->
+      <br>
+      <h2> UI draft</h2>
+      <br>
+      <img :src="imageUrl" alt="Image" width="500">
+
+
 
 
 
@@ -27,3 +37,15 @@
   <style>
 
   </style>
+
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      imageUrl: 'src/assets/pictures/card_design.jpg' // Replace with the URL of your image
+    };
+  }
+}
+</script>
